2b14e0fb705e83842bc07bf77089c5fb20ea26ed
João Fonseca <1231138@isep.ipp.pt>
2024-05-03 17:22:31 +0100

    Pprog US002 (Second Update)

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/RegisterJobController.java
@@ -0,0 +1,26 @@
+package pt.ipp.isep.dei.esoft.project.application.controller;
+
+import pt.ipp.isep.dei.esoft.project.domain.Job;
+import pt.ipp.isep.dei.esoft.project.repository.JobRepository;
+import pt.ipp.isep.dei.esoft.project.repository.Repositories;
+
+
+import java.util.Optional;
+
+public class RegisterJobController {
+
+    private final JobRepository jobRepository;
+
+
+    public RegisterJobController() {
+        this.jobRepository = Repositories.getInstance().getJobRepository();
+
+    }
+
+    public Optional<Job> registerSkill(String name) {
+        return jobRepository.add(name);
+    }
+
+    public Optional<Job> registerJob(String name) {
+    }
+}
---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/RegisterJobUI.java
@@ -0,0 +1,47 @@
+package pt.ipp.isep.dei.esoft.project.ui.console;
+
+import pt.ipp.isep.dei.esoft.project.application.controller.RegisterJobController;
+import pt.ipp.isep.dei.esoft.project.domain.Job;
+import pt.ipp.isep.dei.esoft.project.ui.console.utils.Utils;
+
+import java.util.Optional;
+
+public class RegisterJobUI implements Runnable {
+
+    private final RegisterJobController ctrl = new RegisterJobController();
+    private String name;
+
+    public void run() {
+        System.out.println("\n >>>>>>>>>> REGISTER JOB <<<<<<<<<< \n");
+
+        name = Utils.readLineFromConsole("Insert the job name:");
+
+        validate();
+
+    }
+
+    private void validate() {
+        System.out.println(">>>>>>>>>> JOB NAME <<<<<<<<<< \n");
+
+        System.out.printf("Job name: %s\n", name);
+
+        System.out.println();
+
+        boolean input = Utils.confirm("Do you wish to proceed? (s or n):\n");
+
+        if (input) {
+            System.out.println("Registration finalized\n");
+            Optional<Job> job = ctrl.registerJob(name);
+            if (job.isPresent()){
+                System.out.println("Job registration successful.");
+
+            } else {
+                System.out.printf("Invalid Job.");
+
+            }
+        } else {
+            System.out.println("Cancelling registration\n");
+            run();
+        }
+    }
+}
